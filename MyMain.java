80f9e025b361e57faf22b0fcae8609fe6de990cb
kiranindu <kiran.budida@gmail.com>
2019-01-16 11:56:54 +0530

    added println in main method

--- MyMain.java
+++ MyMain.java
@@ -1,5 +1,6 @@
 public class MyMain{
 public static void main(String[] args){
-System.out.println("Hello world!!");
+System.out.println("From class:MyMain, text:Hello world!!");
+System.out.println("Hello world...");
 }
 }
